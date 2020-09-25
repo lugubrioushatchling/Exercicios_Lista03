cf948458f0985020706ce55b1d901b138226a4b3
lugubrioushatchling <37968162+lugubrioushatchling@users.noreply.github.com>
2020-09-25 10:21:51 -0300

    Create Ex02.c

---
+++ Ex02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int i;
+    char array1[50];
+    char array2[50];
+    printf("Digite dois elementos: ");
+    scanf("%s", array1);
+    scanf("%s", array2);
+    for (i = 0; array1[i]!=0; i++){
+        if(array1[i] != array2[i]){
+            printf("Sao diferentes.");
+            return 0;
+        }
+    }
+    printf("Sao iguais!");
+    return 0;
+}
